7e239db7bb4a108eddf3740020f2fd98c47d7da5
Matthew Rahtz <matthew.rahtz@gmail.com>
2015-05-31 12:24:46 +0100

    Plot classification distribution of breast cancer data

--- module3.py
+++ module3.py
@@ -1,25 +1,78 @@
+#!/usr/bin/env python
+
+"""
+Plot distribution and correlation of feature data from breast cancer data set
+"""
+
+from __future__ import print_function
 import csv
 import numpy
 import scipy
+import scipy.stats
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
-from sklearn.cross_validation import train_test_split
-from sklearn import metrics
 
-#Imports for Module 4
-from sklearn import neighbors
-import knnplots
+def load_data_set():
+    """
+    Load the UCI breast cancer data set.
+    (https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic))
+    """
+    data_set_file_name = "wdbc.csv"
+    with open(data_set_file_name, "r") as data_set_file:
+        # csv.reader returns an interator which must be converted into a list first
+        data_set_list = list(csv.reader(data_set_file))
 
+    data_set = numpy.asarray(data_set_list)
+    # always examine data before starting!
+    # (data may have silently loaded in the wrong format, or truncated)
+    # use (tuple,) to form single tuple from original tuple
+    print("Shape of data set is:\n%s" % (data_set.shape,))
+    print("First row of data set is:\n%s" % data_set[0, :])
+    # second column is benign/malignant classification
+    classification_data = data_set[:, 1]
+    # ignore first two columns (sample ID and classification)
+    # to get feature data
+    feature_data = data_set[:, 2:].astype(float)
+    print("Shape of X matrix (feature data) is:\n%s" %
+        (feature_data.shape,))
+    print("Shape of y vector (classifications) is:\n%s\n" %
+        (classification_data.shape,))
 
-#Code common to all modeles from module 3 onwards
-##NB. The X and yTransformed variables come from the preprocessing in the previous module.
-fileName = "wdbc.csv"
-fileOpen = open(fileName, "rU")
-csvData = csv.reader(fileOpen)
-dataList = list(csvData)
-dataArray =  numpy.array(dataList)
-X = dataArray[:,2:32].astype(float)
-y = dataArray[:, 1]
-le = preprocessing.LabelEncoder()
-le.fit(y)
-yTransformed = le.transform(y)
+    return feature_data, classification_data
+
+def plot_classification_distribution(classification_data):
+    """
+    Plot the distribution of the benign/malignant distribution
+    using bar charts.
+    """
+    classification_frequency = scipy.stats.itemfreq(classification_data)
+
+    for row_n, row in enumerate(classification_frequency):
+        if row[0] == 'B':
+            label = 'Benign'
+            color = 'b'
+        elif row[0] == 'M':
+            label = 'Malignant'
+            color = 'r'
+        else:
+            raise Exception("Unkown classification:", row[0])
+        frequency = int(row[1])
+        plt.bar(left=row_n, height=frequency, color=color, label=label)
+
+    plt.gca().axes.xaxis.set_ticklabels([])
+    plt.legend()
+    plt.xlabel("Diagnosis")
+    plt.ylabel("Frequency")
+    print(
+        "In order to have our classifier be adept at spotting all classes,\n"
+        "we must ensure our data has a reasonably equal distribution.\n"
+    )
+    plt.show()
+
+def main():
+    """
+    Main function of the script.
+    """
+    feature_data, classification_data = load_data_set()
+    plot_classification_distribution(classification_data)
+
+main()
