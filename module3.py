394df90cb6264192abac125a516df0513ac53f32
Matthew Rahtz <matthew.rahtz@gmail.com>
2015-05-31 13:45:31 +0100

    Refactor breast cancer data loading into separate module

--- module3.py
+++ module3.py
@@ -5,39 +5,11 @@ Plot distribution and correlation of feature data from breast cancer data set.
 """
 
 from __future__ import print_function
-import csv
 import numpy
 import scipy
 import scipy.stats
 import matplotlib.pyplot as plt
-
-def load_data_set():
-    """
-    Load the UCI breast cancer data set.
-    (https://archive.ics.uci.edu/ml/datasets/Breast+Cancer+Wisconsin+(Diagnostic))
-    """
-    data_set_file_name = "wdbc.csv"
-    with open(data_set_file_name, "r") as data_set_file:
-        # csv.reader returns an interator which must be converted into a list first
-        data_set_list = list(csv.reader(data_set_file))
-
-    data_set = numpy.asarray(data_set_list)
-    # always examine data before starting!
-    # (data may have silently loaded in the wrong format, or truncated)
-    # use (tuple,) to form single tuple from original tuple
-    print("Shape of data set is:\n%s" % (data_set.shape,))
-    print("First row of data set is:\n%s" % data_set[0, :])
-    # second column is benign/malignant classification
-    classification_data = data_set[:, 1]
-    # ignore first two columns (sample ID and classification)
-    # to get feature data
-    feature_data = data_set[:, 2:].astype(float)
-    print("Shape of feature data is:\n%s" %
-        (feature_data.shape,))
-    print("Shape of classification data is:\n%s\n" %
-        (classification_data.shape,))
-
-    return feature_data, classification_data
+import wdbc
 
 def plot_classification_frequency(classification_data):
     """
@@ -174,7 +146,7 @@ def main():
     """
     Main function of the script.
     """
-    feature_data, classification_data = load_data_set()
+    feature_data, classification_data = wdbc.load_data_set()
     plot_classification_frequency(classification_data)
     plot_feature_correlation(feature_data)
     plot_feature_distribution(feature_data, classification_data)
